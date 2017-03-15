675bce258d1826c89e833f3b9f0e31d755434ae7
Hguimaraes <hguimaraes@poli.ufrj.br>
2017-03-14 22:41:11 -0300

    Getting mp3 preview from spotify

--- app/models/songlist.js
+++ app/models/songlist.js
@@ -1,6 +1,8 @@
 var config      = require("../../config/config");
 var request     = require('request');
 var querystring = require('querystring');
+var https        = require('https');
+var fs          = require('fs');
 
 var SongList = function(opt, access_token){
   this.opt = opt;
@@ -9,11 +11,12 @@ var SongList = function(opt, access_token){
 };
 
 SongList.prototype.getTracks = function(genre, callback){
+  var self = this;
   var offset = 0;
   var search_limit = 50;
 
   // Get N tracks in batchs of 50 per request (Limited by Spotify)
-  for (var i = 0; i < (this.opt.maxNumMusic/search_limit); i++) {
+  for (var i = 0; i < (self.opt.maxNumMusic/search_limit); i++) {
     // Header parameters for get tracks request
     var options_searchtrack = {
       url: config.spotify.url.search_item + querystring.stringify({
@@ -23,7 +26,7 @@ SongList.prototype.getTracks = function(genre, callback){
         offset: offset
       }),
       headers: { 
-        'Authorization': 'Bearer ' + this.access_token
+        'Authorization': 'Bearer ' + self.access_token
       },
       json: true
     };
@@ -34,17 +37,56 @@ SongList.prototype.getTracks = function(genre, callback){
       if(!error && response.statusCode == 200){
         // Received requests
         var tracks = body.tracks.items;
+
+        // ID List
+        var track_id = [];
         
         // Parse the result of the request
         tracks.map(function(value, index){
-          console.log(value.id + ", " + value.preview_url + ", " + genre);
+          // Save the tracks Id
+          track_id.push(value.id);
+
+          // If the Download Preview is selected
+          if(self.opt.audioprev){
+            var file_path = "./dataset/" + genre + "/" + value.id + ".mp3"
+            downloadPreviewTrack(file_path, value.preview_url);
+          }
         });
+
+        // If the Audio metadata is selected
+        // Request audio-features for a set of ids
+        if(self.opt.audiomet){
+          console.log(track_id);
+        }
+
+        // If backup is selected
+        if(self.opt.bkp){
+          console.log(self.opt);
+        }
       }
     });
 
     // Increment offset for the next batch
     offset = offset + search_limit;
   }
+};
+
+
+// Thanks to the awesome answer of Vince Yuan on Stackoverflow
+var downloadPreviewTrack = function(file_path, preview_url, callback){
+  // Create the file and download from the given URL
+  var file = fs.createWriteStream(file_path);
+  var request = https.get(preview_url, function(response) {
+    response.pipe(file);
+    
+    file.on('finish', function() {
+      file.close(callback);
+    });
+
+  }).on('error', function(err) { // Handle errors
+    fs.unlink(dest); // Delete the file async.
+    if (callback) callback(err.message);
+  });
 };
 
 module.exports = SongList; 
