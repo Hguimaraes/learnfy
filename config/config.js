627cd62dfd15f22216ce756615a554c630cf7e04
Hguimaraes <hguimaraes@poli.ufrj.br>
2017-03-18 11:50:30 -0300

    Constants to config file

--- config/config.js
+++ config/config.js
@@ -8,9 +8,14 @@ module.exports = {
     'url': {
       'account': 'https://accounts.spotify.com/',
       'refresh_token': 'https://accounts.spotify.com/api/token',
-      'audio_features': 'https://api.spotify.com/v1/audio-features',
+      'audio_features': 'https://api.spotify.com/v1/audio-features/?',
       'search_item' : 'https://api.spotify.com/v1/search?',
       'search_track' : 'https://api.spotify.com/v1/tracks'
     }
+  },
+  'constants':{
+    'audio_preview_folder' : './dataset/songs/',
+    'truth_table_filename' : "./dataset/spotify_truth_table.csv",
+    'dataset_filename' : './dataset/spotify_dataset.csv'
   }
 };
