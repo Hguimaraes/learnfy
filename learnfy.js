04db8a10b451a8f2e1b827293173575c99d95dbd
Hguimaraes <hguimaraes@poli.ufrj.br>
2017-04-01 13:27:30 -0300

    disable morgan. re-add with verbose option in the future

--- learnfy.js
+++ learnfy.js
@@ -38,7 +38,7 @@ app.use(function(req, res, next) {
 });
 
 // log all requests to the console 
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 
 // Setting port and static path to express
 app.use(express.static(path.join(__dirname, public_folder)));
